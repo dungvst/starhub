560a3626b5a771b0a7cf826109eed7f2686317e0
DungNV <dung.nguyenvan@mhealth.tech>
2017-02-23 00:38:23 +0700

    init

---
+++ src/main/java/com/starhub/configuration/AppConfig.java
@@ -0,0 +1,63 @@
+package com.starhub.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+/**
+ * 
+ * @author DungNV
+ *
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.starhub")
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    /**
+     * Configure TilesConfigurer.
+     */
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions(new String[] { "/WEB-INF/views/**/tiles.xml" });
+        tilesConfigurer.setCheckRefresh(true);
+        return tilesConfigurer;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+    /**
+     * Configure ViewResolvers to deliver preferred views.
+     */
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        TilesViewResolver viewResolver = new TilesViewResolver();
+        registry.viewResolver(viewResolver);
+    }
+
+    /**
+     * Configure ResourceHandlers to serve static resources like CSS/ Javascript
+     * etc...
+     */
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+
+}
---
+++ src/main/java/com/starhub/controller/AppController.java
@@ -0,0 +1,22 @@
+package com.starhub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * @author DungNV
+ *
+ */
+
+@Controller
+@RequestMapping("/")
+public class AppController {
+
+	@RequestMapping(value = { "/"}, method = RequestMethod.GET)
+	public String homePage(ModelMap model) {
+		return "home";
+	}
+}
